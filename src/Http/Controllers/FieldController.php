abe73962293b4a55089dbe7236d89c47f4eac33e
Rick Lambrechts <rick.lambrechts@hotmail.com>
2022-06-21 15:07:09 +0200

    Use uri keys and send current resource in request so callback can use it (#117)

--- src/Http/Controllers/FieldController.php
+++ src/Http/Controllers/FieldController.php
@@ -2,11 +2,14 @@
 
 namespace Orlyapps\NovaBelongsToDepend\Http\Controllers;
 
+use Illuminate\Database\Eloquent\Model;
 use \Illuminate\Http\Resources\MergeValue;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Collection;
 use Laravel\Nova\Http\Requests\NovaRequest;
+use Laravel\Nova\Nova;
 use Laravel\Nova\Panel;
+use Laravel\Nova\Resource;
 use Orlyapps\NovaBelongsToDepend\NovaBelongsToDepend;
 
 class FieldController extends Controller
@@ -17,10 +20,14 @@ class FieldController extends Controller
             abort(500, 'Depend On Relationship not found on the Resource specified for the Field "' . $request->attribute . '" Please check you have set correct /App/Nova/Resource');
         }
 
-        if (method_exists($request->resourceClass, 'newModel')) {
-            $resource = new $request->resourceClass($request->resourceClass::newModel());
-        } else {
-            $resource = new $request->resourceClass;
+        $resource = Nova::resourceInstanceForKey($request->resource);
+        if (is_null($resource)) {
+            abort(500, 'Could not find resource "' . $request->resource);
+        }
+
+        $resourceModel = $resource->model()->find($request->resourceId);
+        if (is_null($resource)) {
+            abort(500, 'Could not find resource "' . $request->resource . '" with ' . $request->resourceId);
         }
 
         $attributedField = $this->getAttributedField($request, $resource);
@@ -31,19 +38,27 @@ class FieldController extends Controller
             return [];
         }
 
-        $options = $this->getOptions($attributedField, $modelMap);
+        $options = $this->getOptions($attributedField, $modelMap, $resourceModel);
 
         return $options instanceof Collection ? $options : [$options];
     }
 
-    public function returnFields($fields)
+    /**
+     * @param array $fields
+     * @return Collection
+     */
+    public function returnFields(array $fields)
     {
         return collect($fields)->map(function ($field) {
             if (isset($field->data)) {
                 return $this->returnFields($field->data);
-            } elseif (isset($field->meta['fields'])) {
+            }
+
+            if (isset($field->meta['fields'])) {
                 return $this->returnFields($field->meta['fields']);
-            } elseif (isset($field->fields)) {
+            }
+
+            if (isset($field->fields)) {
                 return $this->returnFields($field->fields);
             }
 
@@ -51,36 +66,43 @@ class FieldController extends Controller
         })->flatten();
     }
 
-    private function getAttributedField($request, $resource)
+    /**
+     * @param NovaRequest $request
+     * @param Resource $resource
+     * @return NovaBelongsToDepend
+     */
+    private function getAttributedField(NovaRequest $request, Resource $resource)
     {
         // Create Nested Array Fields from Panels, filter out irrelevant fields
         $fields = $this->returnFields($resource->fields($request));
 
-        $fields = $fields->filter(
-            function ($value) use ($request) {
-                return ($value instanceof NovaBelongsToDepend);
-            }
-        );
+        $fields = $fields->filter(function ($value) {
+            return ($value instanceof NovaBelongsToDepend);
+        });
 
-        //get the attributed field
+        // get the attributed field
         $attributedField = $fields->first(function ($value, $key) use ($request) {
             return ($value instanceof NovaBelongsToDepend && $value->attribute == $request->attribute);
         });
 
         if (is_null($attributedField)) {
-            abort(500, 'Can not find the Field "' . $request->attribute . '" in the Model "' . $request->resourceClass . '"');
+            abort(500, 'Can not find the Field "' . $request->attribute . '" in the Model "' . $resource::class . '"');
         }
 
         return $attributedField;
     }
 
-    private function getModelMap($request, $attributedField)
+    /**
+     * @param NovaRequest $request
+     * @param NovaBelongsToDepend $attributedField
+     * @return array
+     */
+    private function getModelMap(NovaRequest $request, NovaBelongsToDepend $attributedField)
     {
         $models = [];
 
         foreach ($request->dependsMap as $value) {
-
-            $modelClass = $value['key'] ?? $request->modalClass;
+            $modelClass = Nova::modelInstanceForKey($value['key'] ?? $request->modalClass);
             if (is_null($modelClass::find($value['value']))) {
                 abort(500, 'Can not find the Model "' . $modelClass . '::find(' . $value['value'] . ')');
             }
@@ -99,17 +121,15 @@ class FieldController extends Controller
         return $modelMap;
     }
 
-    private function getOptions($attributedField, $modelMap)
+    private function getOptions(NovaBelongsToDepend $attributedField, array $modelMap, Model $resourceModel)
     {
         $options = null;
 
         if (count($modelMap) == 1 && count($attributedField->dependsOn) == 1) {
-
             $model = $modelMap[$attributedField->dependsOn[0]];
-            $options = ($attributedField->optionResolveCallback)($model);
-
+            $options = ($attributedField->optionResolveCallback)($model, $resourceModel);
         } else {
-            $options = ($attributedField->optionResolveCallback)((object)$modelMap);
+            $options = ($attributedField->optionResolveCallback)((object)$modelMap, $resourceModel);
         }
 
         if (is_null($options)) {
